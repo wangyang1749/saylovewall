8b94874f08bf76f8e1bf0522252c5b57ddf564d0
wangyang <1749748955@qq.com>
2018-08-20 20:08:42 +0800

    add baseservlet

---
+++ src/com/yiban/servlet/BaseServlet.java
@@ -0,0 +1,22 @@
+package com.yiban.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class BaseServlet extends HttpServlet {
+
+	/**
+	 * mvc的设计模式
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+		System.out.println("被调用");
+		super.service(arg0, arg1);
+	}
+}
